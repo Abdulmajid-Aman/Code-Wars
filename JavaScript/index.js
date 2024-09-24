f2a468d6069a060965bd58ef60ae8da735403be1
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-24 19:26:42 +0300

    Code Wars Solutions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -517,8 +517,54 @@ console.log(outed({"tim":1,"jim":8,"randy":3,"sandy":7,"andy":3,"katie":5,"laura
 
 
 
+/* Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+ */
+
+function getCount(str) {
+  let count = 0
+  let vowels = 'aeiou'
+  let word = str.split("")
+  for(let i = 0; i< word.length; i ++) {
+   for(let letter of vowels) {
+    if(word[i] === letter) {
+      count++
+    }
+   }
+  }
+  console.log(count);
+  
+}
+
+getCount("abracadabra")
 
 
+
+/* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+Input: "1 2 3 4 5"   =>  Output: "5 1"
+Input: "1 2 -3 4 5"  =>  Output: "5 -3"
+Input: "1 9 3 4 -5"  =>  Output: "9 -5"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first. */
+
+
+function highAndLow(numbers) {
+  let num = numbers.split(" ")
+  let maxValue = Math.max(...num)
+  let minValue = Math.min(...num)
+  return `${maxValue} ${minValue}`
+}
+
+
+console.log(highAndLow("1 2 3 4 5" ));
 
 
 
